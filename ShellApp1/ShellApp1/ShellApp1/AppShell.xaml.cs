d011ee1dc7a609538bcfec237bb2f749e102b8d8
wzdr <wzdrenka@gmail.com>
2019-07-27 21:19:21 +1200

    added source files via upload

---
+++ ShellApp1/ShellApp1/ShellApp1/AppShell.xaml.cs
@@ -0,0 +1,44 @@
+﻿using ShellApp1.Views;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ShellApp1
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        private Dictionary<string, Type> routes = new Dictionary<string, Type>();
+
+        public ICommand AboutCommand => new Command(async () =>
+        {
+            await Shell.Current.GoToAsync("about", true);
+            Shell.Current.FlyoutIsPresented = false;  //retract flyout
+        });
+
+        public ICommand SettingsCommand => new Command(async () =>
+        {
+            // do something...
+            await Task.Delay(1000);
+            Shell.Current.FlyoutIsPresented = false;  //retract flyout
+        });
+
+        public AppShell()
+        {
+            InitializeComponent();
+            RegisterRoutes();
+            BindingContext = this;
+        }
+
+        private void RegisterRoutes()
+        {
+            routes.Add("about", typeof(AboutPage));
+
+            foreach (var item in routes)
+            {
+                Routing.RegisterRoute(item.Key, item.Value);
+            }
+        }
+    }
+}
---
+++ ShellApp1/ShellApp1/ShellApp1/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using ShellApp1.Models;
+
+namespace ShellApp1.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
